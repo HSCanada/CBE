080ec9fd394d9d0d3a1884ee11c65058ef22d980
trevor-crowley <trevor.crowley@henryschein.ca>
2016-01-20 20:03:43 -0500

    store prod view and proc scripts

---
+++ procs/Booking.sql
@@ -0,0 +1,43 @@
+
+-- FSC
+
+UPDATE    
+	comm_transaction
+SET              
+	gp_ext_amt = comm_transaction.transaction_amt * (g.booking_rt / 100.0)
+FROM         
+	comm_transaction 
+	
+	INNER JOIN comm_group AS g 
+	ON comm_transaction.comm_group_cd = g.comm_group_cd
+
+WHERE     
+	(g.booking_act_ind = 1) AND 
+	(comm_transaction.fiscal_yearmo_num between '201512' and '201512')
+
+-- ESS
+
+GO
+
+UPDATE    
+	comm_transaction
+SET              
+	gp_ext_amt = comm_transaction.transaction_amt * (g.booking_rt / 100.0)
+FROM         
+	comm_transaction 
+	
+	INNER JOIN comm_group AS g 
+	ON comm_transaction.ess_comm_group_cd = g.comm_group_cd
+
+WHERE     
+	(g.booking_act_ind = 1) AND 
+	(comm_transaction.fiscal_yearmo_num between '201512' and '201512')
+
+GO
+
+/*
+SELECT     comm_transaction.comm_group_cd, comm_transaction.transaction_amt, comm_transaction.gp_ext_amt, comm_transaction.gp_ext_amt / comm_transaction.transaction_amt, g.booking_rt
+FROM         comm_transaction INNER JOIN
+                      comm_group AS g ON comm_transaction.comm_group_cd = g.comm_group_cd
+WHERE     (g.booking_act_ind = 1) AND (comm_transaction.fiscal_yearmo_num = '201412') AND comm_transaction.transaction_amt <> 0
+*/
