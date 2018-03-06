39701e97b173f82ad79062417636789144d7ea96
Crowley <Trevor.Crowley@henryschein.ca>
2018-03-06 14:59:48 -0500

    fsc & ccs bonus logic 2018

--- SourceSql/comm_ess_statement_export02.sql
+++ SourceSql/comm_ess_statement_export02.sql
@@ -22,80 +22,53 @@ GO
 **	27 Jan 10	tmc		Updated for Comm 2010
 **	02 Dec 10	tmc		Trim SalespersonID and BranchNM
 **  01 Apr 13	tmc		Add ESS commission plan (to filter out retired plans
+**	06 Mar 18	tmc		Add Branch Material to CCS bonus
 **    
 *******************************************************************************/
 
 ALTER VIEW [dbo].[comm_ess_statement_export02]
 AS
 SELECT   
-	m.employee_num,
-	RTRIM(m.salesperson_key_id) as salesperson_key_id, 
-	m.salesperson_nm, 
-	m.hsi_salesperson_cd,
+	''	AS ccs_employee_num
+	,'' AS ccs_salesperson_key_id
+	,'' AS ccs_salesperson_nm
+	,'' AS ccs_salesperson_cd
+	,[branch_nm] 
 
 	-- Group (01)= BONPEL
-	BONPEL_SALES_CM_AMT,
-	BONPEL_SALES_YTD_AMT,
-	BONPEL_GP_CM_AMT,
-	BONPEL_GP_YTD_AMT,
-
-
-	-- Group (02)= BONKAV
-	BONKAV_SALES_CM_AMT,
-	BONKAV_SALES_YTD_AMT,
-	BONKAV_GP_CM_AMT,
-	BONKAV_GP_YTD_AMT,
-
-	-- Group (03)= BONSIR
-	BONSIR_SALES_CM_AMT,
-	BONSIR_SALES_YTD_AMT,
-	BONSIR_GP_CM_AMT,
-	BONSIR_GP_YTD_AMT,
-
-	-- Added 1 Apr 13
-	m.comm_plan_id as ESS_comm_plan_id
+	,BON_DIGMAT_SALES_CM_AMT
+	,BON_DIGMAT_SALES_YTD_AMT
+	,BON_DIGMAT_GP_CM_AMT
+	,BON_DIGMAT_GP_YTD_AMT
 
 
 FROM         
 	(
 	SELECT     
-		ss.salesperson_key_id, 
+		branch_nm, 
 
 		-- Group (01)= BONPEL
-		Sum(Case When comm_group_cd = 'BONPEL' Then ss.sales_curr_amt Else 0 End) as					BONPEL_SALES_CM_AMT,
-		Sum(Case When comm_group_cd = 'BONPEL' Then ss.sales_ytd_amt + ss.sales_curr_amt Else 0 End) as BONPEL_SALES_YTD_AMT,
-		Sum(Case When comm_group_cd = 'BONPEL' Then ss.gp_curr_amt Else 0 End) as						BONPEL_GP_CM_AMT,
-		Sum(Case When comm_group_cd = 'BONPEL' Then ss.gp_ytd_amt + ss.gp_curr_amt Else 0 End) as		BONPEL_GP_YTD_AMT,
-
-
-		-- Group (02)= BONKAV
-		Sum(Case When comm_group_cd = 'BONKAV' Then ss.sales_curr_amt Else 0 End) as					BONKAV_SALES_CM_AMT,
-		Sum(Case When comm_group_cd = 'BONKAV' Then ss.sales_ytd_amt + ss.sales_curr_amt Else 0 End) as BONKAV_SALES_YTD_AMT,
-		Sum(Case When comm_group_cd = 'BONKAV' Then ss.gp_curr_amt Else 0 End) as						BONKAV_GP_CM_AMT,
-		Sum(Case When comm_group_cd = 'BONKAV' Then ss.gp_ytd_amt + ss.gp_curr_amt Else 0 End) as		BONKAV_GP_YTD_AMT,
-
-		-- Group (03)= BONSIR
-		Sum(Case When comm_group_cd = 'BONSIR' Then ss.sales_curr_amt Else 0 End) as					BONSIR_SALES_CM_AMT,
-		Sum(Case When comm_group_cd = 'BONSIR' Then ss.sales_ytd_amt + ss.sales_curr_amt Else 0 End) as BONSIR_SALES_YTD_AMT,
-		Sum(Case When comm_group_cd = 'BONSIR' Then ss.gp_curr_amt Else 0 End) as						BONSIR_GP_CM_AMT,
-		Sum(Case When comm_group_cd = 'BONSIR' Then ss.gp_ytd_amt + ss.gp_curr_amt Else 0 End) as		BONSIR_GP_YTD_AMT
-
-
+		Sum(Case When comm_group_cd = 'DIGMAT' Then ss.sales_curr_amt Else 0 End) as					BON_DIGMAT_SALES_CM_AMT,
+		Sum(Case When comm_group_cd = 'DIGMAT' Then ss.sales_ytd_amt + ss.sales_curr_amt Else 0 End) as BON_DIGMAT_SALES_YTD_AMT,
+		Sum(Case When comm_group_cd = 'DIGMAT' Then ss.gp_curr_amt Else 0 End) as						BON_DIGMAT_GP_CM_AMT,
+		Sum(Case When comm_group_cd = 'DIGMAT' Then ss.gp_ytd_amt + ss.gp_curr_amt Else 0 End) as		BON_DIGMAT_GP_YTD_AMT
 
 	FROM         
 		comm_summary AS ss 
+
+		INNER JOIN comm_salesperson_master m
+		ON ss.salesperson_key_id = m.salesperson_key_id 
+
+		INNER JOIN dbo.comm_branch AS b
+		ON m.branch_cd = b.branch_cd 
+
 	WHERE   
-		comm_group_cd <> '' and
-		salesperson_key_id <> ''
+		(m.comm_plan_id like 'FSC%') AND
+		ss.salesperson_key_id <> 'Internal'
 	GROUP BY 
-		ss.salesperson_key_id 
+		branch_nm
 	) as s
  
-		INNER JOIN comm_salesperson_master m
-		ON s.salesperson_key_id = m.salesperson_key_id 
-
-WHERE     
-	(m.comm_plan_id like 'ESS%')
 GO
 
 SET ANSI_NULLS OFF
@@ -103,3 +76,4 @@ GO
 SET QUOTED_IDENTIFIER OFF
 GO
 
+-- SELECT TOP 10 * FROM [comm_ess_statement_export02]
--- SourceSql/comm_statement_export02.sql
+++ SourceSql/comm_statement_export02.sql
@@ -24,40 +24,31 @@ GO
 **	28 Mar 11	tmc		switch Biolase bonus with BDMs
 --  30 Jan 16	tmc		Update for new comm codes
 --  04 Apr 17	tmc		Update for 2017 bonus -- scanners and mills
+--	06 Mar 18	tmc		Update for 2018 FSC bonus -- scanners and mills
 *******************************************************************************/
 
 ALTER VIEW [dbo].[comm_statement_export02]
 AS
 SELECT   
-	m.employee_num,
-	RTRIM(m.salesperson_key_id) as salesperson_key_id, 
-	m.salesperson_nm, 
-	RTRIM(master_salesperson_cd) AS hsi_salesperson_cd,
+	m.employee_num
+	,RTRIM(m.salesperson_key_id) as salesperson_key_id
+	,m.salesperson_nm
+	,RTRIM(master_salesperson_cd) AS hsi_salesperson_cd
 
-	-- Group (02)= BONE4D
-	BONSCNMIL_SALES_CM_AMT,
-	BONSCNMIL_SALES_YTD_AMT,
-	BONSCNMIL_GP_CM_AMT,
-	BONSCNMIL_GP_YTD_AMT,
-	BONSCNMIL_QTY_CM_AMT,
-	BONSCNMIL_QTY_YTD_AMT
+	,BONSCNMIL_SALES_CM_AMT
+	,BONSCNMIL_SALES_YTD_AMT
+	,BONSCNMIL_GP_CM_AMT
+	,BONSCNMIL_GP_YTD_AMT
 
 FROM         
 	(
 	SELECT     
 		ss.salesperson_key_id, 
 
-		-- Group (02)= BONE4D
-		Sum(Case When comm_group_cd = 'BONIMP' Then ss.sales_curr_amt Else 0 End) as					BONSCNMIL_SALES_CM_AMT,
-		Sum(Case When comm_group_cd = 'BONIMP' Then ss.sales_ytd_amt + ss.sales_curr_amt Else 0 End) as BONSCNMIL_SALES_YTD_AMT,
-		Sum(Case When comm_group_cd = 'BONIMP' Then ss.gp_curr_amt Else 0 End) as						BONSCNMIL_GP_CM_AMT,
-		Sum(Case When comm_group_cd = 'BONIMP' Then ss.gp_ytd_amt + ss.gp_curr_amt Else 0 End) as		BONSCNMIL_GP_YTD_AMT,
-
-		-- Use Cost for Qty field
-		Sum(Case When comm_group_cd = 'BONIMP' Then ss.costs_curr_amt Else 0 End) as					BONSCNMIL_QTY_CM_AMT,
-		Sum(Case When comm_group_cd = 'BONIMP' Then ss.costs_ytd_amt + ss.costs_curr_amt Else 0 End) as	BONSCNMIL_QTY_YTD_AMT
-
-
+		Sum(CASE WHEN comm_group_cd IN ('DIGIMP', 'DIGCCS', 'DIGLAB', 'DIGOTH', 'DIGCIM', 'DIGCCC') Then ss.sales_curr_amt Else 0 End)						AS BONSCNMIL_SALES_CM_AMT,
+		Sum(CASE WHEN comm_group_cd IN ('DIGIMP', 'DIGCCS', 'DIGLAB', 'DIGOTH', 'DIGCIM', 'DIGCCC') THEN ss.sales_ytd_amt + ss.sales_curr_amt Else 0 End)	AS BONSCNMIL_SALES_YTD_AMT,
+		Sum(CASE WHEN comm_group_cd IN ('DIGIMP', 'DIGCCS', 'DIGLAB', 'DIGOTH', 'DIGCIM', 'DIGCCC') THEN ss.gp_curr_amt Else 0 End)							AS BONSCNMIL_GP_CM_AMT,
+		Sum(CASE WHEN comm_group_cd IN ('DIGIMP', 'DIGCCS', 'DIGLAB', 'DIGOTH', 'DIGCIM', 'DIGCCC') THEN ss.gp_ytd_amt + ss.gp_curr_amt Else 0 End)			AS BONSCNMIL_GP_YTD_AMT
 
 	FROM         
 		comm_summary AS ss 
